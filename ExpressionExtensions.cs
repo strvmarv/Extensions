26aa80db0b413fdb2f178198fd44cbd2841e34fd
strvmarv <strvmarv@users.noreply.github.com>
2014-10-23 07:36:36 -0500

    Update ExpressionExtensions.cs

--- ExpressionExtensions.cs
+++ ExpressionExtensions.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace Ctc.VisualCactus.Extensions
+namespace Extensions
 {
     public static class ExpressionExtensions
     {
