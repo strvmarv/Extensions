495d716f12ef797b13a9d24bc76d2df150105d65
strvmarv <strvmarv@users.noreply.github.com>
2014-10-23 07:33:50 -0500

    Update DateTimeExtensions.cs

--- DateTimeExtensions.cs
+++ DateTimeExtensions.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Ctc.VisualCactus.Extensions
+namespace Extensions
 {
     public static class DateTimeExtensions
     {
