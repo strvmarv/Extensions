e7aad6e13c6885c3fdbfed8572dd27a8e109e3ad
strvmarv <strvmarv@gmail.com>
2014-10-29 14:03:54 -0500

    Added DictionaryExtensions

---
+++ DictionaryExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    public static class DictionaryExtensions
+    {
+        // Credit: LukeH
+        // Source: http://stackoverflow.com/a/3928856/578859
+        // License: https://creativecommons.org/licenses/by-sa/3.0/
+        public static bool DictionaryEqual<TKey, TValue>(this IDictionary<TKey, TValue> first, IDictionary<TKey, TValue> second)
+        {
+            if (first == second) return true;
+            if ((first == null) || (second == null)) return false;
+            if (first.Count != second.Count) return false;
+
+            var comparer = EqualityComparer<TValue>.Default;
+
+            foreach (KeyValuePair<TKey, TValue> kvp in first)
+            {
+                TValue secondValue;
+                if (!second.TryGetValue(kvp.Key, out secondValue)) return false;
+                if (!comparer.Equals(kvp.Value, secondValue)) return false;
+            }
+            return true;
+        }
+    }
+}
